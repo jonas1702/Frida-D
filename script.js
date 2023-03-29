41d00747bbf84b007808d9e7e4b9feb0179c6c7e
jonas1702 <Jonaskrebber074@gmail.com>
2023-03-29 13:59:35 +0200

    edit

---
+++ script.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector('.hamburger-menu')
+const mobile_expand = document.querySelector('.nav-list--mobile')
+let expanded = false
+
+menu.addEventListener('click', () => {
+    expanded = !expanded
+    if (expanded == false) {
+        mobile_expand.style.display = 'none'
+    }
+    if (expanded == true) {
+        mobile_expand.style.display = 'block'
+    }
+    
+})
