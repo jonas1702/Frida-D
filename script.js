1252ab4843990b58b99ece42cf3037d72cf7b6ba
jonas1702 <Jonaskrebber074@gmail.com>
2023-05-28 23:20:06 +0200

    nav mobile 28.5 1h

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const menu = document.querySelector('.hamburger-menu')
+const blur = document.querySelector('.blur')
 const mobile_expand = document.querySelector('.nav-list--mobile')
 let expanded = false
 
 menu.addEventListener('click', () => {
     expanded = !expanded
     if (expanded == false) {
-        mobile_expand.style.display = 'none'
+        mobile_expand.style.top = '-500px'
+        blur.style.display = 'none'
     }
     if (expanded == true) {
-        mobile_expand.style.display = 'block'
+        mobile_expand.style.top = '90px'
+        setTimeout( function () {
+            blur.style.display = 'block' },200);
     }
     
 })
 
 function handleSubmit() {
-    
+    return
 }
 
 
