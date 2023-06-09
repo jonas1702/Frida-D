033a708d7b0da97ba3d211fb535e04d18253f911
jonas1702 <Jonaskrebber074@gmail.com>
2023-06-09 12:38:31 +0200

    9.6 2 h login fast fertig

--- script.js
+++ script.js
@@ -108,3 +108,41 @@ Array.from(document.querySelectorAll('[name=up]')).forEach(element => {
 
 document.querySelector('[name=contact]').addEventListener('click', () => window.scrollTo(0, 1060))
 document.querySelector('[name=pricing]').addEventListener('click', () => window.scrollTo(0, 1800))
+
+/* login slider */
+
+const loginSlide = Array.from(document.querySelectorAll('.login-swipe-card'))
+
+index = 100
+
+function move() {
+    loginSlide.forEach(card => {
+        card.style.transform = `translateX(-${index}%)`
+    })
+    
+    console.log(index)
+    if (index == 300) {
+        loginSlide.forEach(card => {
+            card.style.transform = `translateX(0)`
+            index = 0
+        })
+    }
+    index += 100
+}
+
+setInterval(move, 10000)
+
+
+function handleLogin() {
+    
+    let forms = Array.from(document.querySelectorAll('.login-interface-form-field'))
+    forms.forEach(form => {
+        console.log(form.value.length)
+        if (form.value.length == 0) {
+            form.classList.add('form-field--wrong')
+        } else {
+            form.classList.remove('form-field--wrong')
+        }
+    })
+}
+
