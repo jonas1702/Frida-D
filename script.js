435fe1acaed5616f29aad5377ed2c9eb1ecf0671
jonas1702 <Jonaskrebber074@gmail.com>
2023-06-20 13:49:07 +0200

    20.06 2h toast, nav, login

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 wkd.autoInit();
 
+/* tab */
+function handleEnter(e){
+    var keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode == '13' || keycode == '32') {
+        document.activeElement.click();
+    }
+}
+
 const menu = document.querySelector('.checkbox')
 const blur = document.querySelector('.blur')
 const mobile_expand = document.querySelector('.nav-list--mobile')
@@ -50,23 +58,35 @@ function getOffset(el) {
   }
 
 
-function handleSubmit() {
+  function handleSubmit() {
     
     let forms = Array.from(document.querySelectorAll('.contact-section-form-field'))
+
+    let submitTrue = true
     forms.forEach(form => {
         if (form.value.length == 0) {
             form.classList.add('form-field--wrong')
+            submitTrue = false
         } else {
             form.classList.remove('form-field--wrong')
         }
     })
-    Toastify({
-        text: "Submit",
-        className: "contact-toast",
-        duration: 3000,
-        close: true,
-        stopOnFocus: true, 
-      }).showToast();
+    if (submitTrue == true) {
+        const newToast = new Toast({
+            text: "Submit successful",
+            position: "top-right",
+            pauseOnHover: true,
+            pauseOnFocusLoss: true,
+          })
+    } else {
+        const newToast = new Toast({
+            text: "Submit failed",
+            position: "top-right",
+            pauseOnHover: true,
+            pauseOnFocusLoss: true,
+          })
+    }
+    
 }
 
 
@@ -76,9 +96,15 @@ const copyElm = Array.from(document.querySelectorAll('.copyElm'))
 
 copyElm.forEach(element => {
     element.addEventListener('click', () => {
-        let copyText = element.querySelector('.copyText')
-        navigator.clipboard.writeText(copyText.innerHTML);
-        alert("Copied the text: " + copyText.innerHTML);
+        console.log('click')
+        let copyText = element
+        navigator.clipboard.writeText(copyText.innerText);
+        const newToast = new Toast({
+            text: "Copied: " + copyText.innerText,
+            position: "top-right",
+            pauseOnHover: true,
+            pauseOnFocusLoss: true,
+          })
     })
 })
 
@@ -158,4 +184,161 @@ function handleLogin() {
         }
     })
 }
+
+/* !!! */
+/* Der code für die toast animations und so kommt von https://www.youtube.com/watch?v=HhpbzPMCKDc weil ich ansonsten keine coolen toast libraries gefunden habe*/
+
+const DEFAULT_OPTIONS = {
+    autoClose: 5000,
+    position: "top-right",
+    onClose: () => {},
+    canClose: true,
+    showProgress: true,
+  }
+  
+    class Toast {
+    #toastElem
+    #autoCloseInterval
+    #progressInterval
+    #removeBinded
+    #timeVisible = 0
+    #autoClose
+    #isPaused = false
+    #unpause
+    #pause
+    #visibilityChange
+    #shouldUnPause
+  
+    constructor(options) {
+      this.#toastElem = document.createElement("div")
+      this.#toastElem.classList.add("toast")
+      requestAnimationFrame(() => {
+        this.#toastElem.classList.add("show")
+      })
+      this.#removeBinded = this.remove.bind(this)
+      this.#unpause = () => (this.#isPaused = false)
+      this.#pause = () => (this.#isPaused = true)
+      this.#visibilityChange = () => {
+        this.#shouldUnPause = document.visibilityState === "visible"
+      }
+      this.update({ ...DEFAULT_OPTIONS, ...options })
+    }
+  
+    set autoClose(value) {
+      this.#autoClose = value
+      this.#timeVisible = 0
+      if (value === false) return
+  
+      let lastTime
+      const func = time => {
+        if (this.#shouldUnPause) {
+          lastTime = null
+          this.#shouldUnPause = false
+        }
+        if (lastTime == null) {
+          lastTime = time
+          this.#autoCloseInterval = requestAnimationFrame(func)
+          return
+        }
+        if (!this.#isPaused) {
+          this.#timeVisible += time - lastTime
+          if (this.#timeVisible >= this.#autoClose) {
+            this.remove()
+            return
+          }
+        }
+  
+        lastTime = time
+        this.#autoCloseInterval = requestAnimationFrame(func)
+      }
+  
+      this.#autoCloseInterval = requestAnimationFrame(func)
+    }
+  
+    set position(value) {
+      const currentContainer = this.#toastElem.parentElement
+      const selector = `.toast-container[data-position="${value}"]`
+      const container = document.querySelector(selector) || createContainer(value)
+      container.append(this.#toastElem)
+      if (currentContainer == null || currentContainer.hasChildNodes()) return
+      currentContainer.remove()
+    }
+  
+    set text(value) {
+      this.#toastElem.textContent = value
+    }
+  
+    set canClose(value) {
+      this.#toastElem.classList.toggle("can-close", value)
+      if (value) {
+        this.#toastElem.addEventListener("click", this.#removeBinded)
+      } else {
+        this.#toastElem.removeEventListener("click", this.#removeBinded)
+      }
+    }
+  
+    set showProgress(value) {
+      this.#toastElem.classList.toggle("progress", value)
+      this.#toastElem.style.setProperty("--progress", 1)
+  
+      if (value) {
+        const func = () => {
+          if (!this.#isPaused) {
+            this.#toastElem.style.setProperty(
+              "--progress",
+              1 - this.#timeVisible / this.#autoClose
+            )
+          }
+          this.#progressInterval = requestAnimationFrame(func)
+        }
+  
+        this.#progressInterval = requestAnimationFrame(func)
+      }
+    }
+  
+    set pauseOnHover(value) {
+      if (value) {
+        this.#toastElem.addEventListener("mouseover", this.#pause)
+        this.#toastElem.addEventListener("mouseleave", this.#unpause)
+      } else {
+        this.#toastElem.removeEventListener("mouseover", this.#pause)
+        this.#toastElem.removeEventListener("mouseleave", this.#unpause)
+      }
+    }
+  
+    set pauseOnFocusLoss(value) {
+      if (value) {
+        document.addEventListener("visibilitychange", this.#visibilityChange)
+      } else {
+        document.removeEventListener("visibilitychange", this.#visibilityChange)
+      }
+    }
+  
+    update(options) {
+      Object.entries(options).forEach(([key, value]) => {
+        this[key] = value
+      })
+    }
+  
+    remove() {
+      cancelAnimationFrame(this.#autoCloseInterval)
+      cancelAnimationFrame(this.#progressInterval)
+      const container = this.#toastElem.parentElement
+      this.#toastElem.classList.remove("show")
+      this.#toastElem.addEventListener("transitionend", () => {
+        this.#toastElem.remove()
+        if (container.hasChildNodes()) return
+        container.remove()
+      })
+      this.onClose()
+    }
+  }
+  
+  function createContainer(position) {
+    const container = document.createElement("div")
+    container.classList.add("toast-container")
+    container.dataset.position = position
+    document.body.append(container)
+    return container
+  }
 
